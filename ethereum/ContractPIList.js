989c7223eccd244b02c79153cbf32aa3ffae20ff
hoangtuleulao <38433892+hoangtuleulao@users.noreply.github.com>
2018-05-03 05:47:06 +0700

    [Insurer] ==> Support to write the deployed address into deployed_address.json ==> Add & import deployed_address.js to write the deployed addresses to the above file.

--- ethereum/ContractPIList.js
+++ ethereum/ContractPIList.js
@@ -1,9 +1,10 @@
 import web3 from './web3';
 import ContractPIList from './build/ContractPIList.json';
+import deployed_address from './deployed_address.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(ContractPIList.interface),
-  '0x8CdaF0CD259887258Bc13a92C0a6dA92698644C0'
+  deployed_address['ContractPIList']
 );
 
 export default instance;
