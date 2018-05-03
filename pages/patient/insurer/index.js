52b1a74670cbbad02283a2c3dd8d715572ad7aed
Edward <edwardthienhoang@gmail.com>
2018-05-03 22:16:46 +0700

    Insurer list: fix display summary status

--- pages/patient/insurer/index.js
+++ pages/patient/insurer/index.js
@@ -11,37 +11,49 @@ import {Pi} from './pi';
 class CampaignIndex extends Component {
 
   static async getInitialProps() {
-	const accounts = await web3.eth.getAccounts();
-    const campaigns = await ContractPIList.methods.getPatientContracts(accounts[1]).call();
+    const accounts = await web3.eth.getAccounts();
+    const campaigns = await 
+		ContractPIList.methods.getPatientContracts(accounts[1]).call();
 
     return { campaigns };
-  }
+   }
 
-  renderCampaigns() {
-    const items = this.props.campaigns.map(address => {
-			var summary = Pi.getSummary(address);
-      return {
-        header: address,
-        description: (
-					<Grid columns='equal' divided>
-						<Grid.Row stretched>
-							<Grid.Column>
-								<Link route={`/patient/insurer/view/${address}`}>
-									<a>View Contract</a>
-								</Link>
-							</Grid.Column>
-							<Grid.Column width={3}>
-								{Pi.renderStatus(summary[0])}
-							</Grid.Column>
-						</Grid.Row>
-					</Grid>
-        ),
-        fluid: true
-      };
-    });
+	 state = {
+			contractStatus: []
+	 };
 
-    return <Card.Group items={items} />;
-  }
+	 async getSummary(address) {
+			 let summary = await Pi.getSummary(address);
+			 var _contractStatus = this.state.contractStatus;
+			 _contractStatus[address] = summary[0];// status (int)
+			 this.setState({contractStatus: _contractStatus});
+	 }
+
+   renderCampaigns() {
+     const items = this.props.campaigns.map(address => {
+			this.getSummary(address);
+       return {
+         header: address,
+         description: (
+					 <Grid columns='equal' divided>
+							 <Grid.Row stretched>
+									 <Grid.Column>
+											 <Link route={`/patient/insurer/view/${address}`}>
+													 <a>View Contract</a>
+											 </Link>
+									 </Grid.Column>
+									 <Grid.Column width={3}>
+											{Pi.renderStatus(this.state.contractStatus[address])}
+									 </Grid.Column>
+							 </Grid.Row>
+					 </Grid>
+         ),
+         fluid: true
+       };
+     });
+
+     return <Card.Group items={items} />;
+   }
 
   render() {
     return (
