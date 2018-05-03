9980c2ac27696d4d2ffa004c5fc85707806f0aca
hoangtuleulao <38433892+hoangtuleulao@users.noreply.github.com>
2018-05-04 05:51:52 +0700

    Use classname instead of class attribute name

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default () => {
   return (
-		<div class="ui vertical footer segment">
-			<div class="ui center aligned container">
-					<h4 class="ui header"></h4>
-					<h4 class="ui header">&copy; Copyright 2018 | All rights reserved | ICT</h4>
+		<div className="ui vertical footer segment">
+			<div className="ui center aligned container">
+					<h4 className="ui header"></h4>
+					<h4 className="ui header">&copy; Copyright 2018 | All rights reserved | ICT</h4>
 			</div>
 		</div>
 		
